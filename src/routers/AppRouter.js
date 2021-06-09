b910fd97913b8f05e093a7bea991dbc7b9bc7057
fiorellaGamarra <fiorella.gamarra@tecsup.edu.pe>
2021-06-09 18:36:14 -0500

    arreglando nuevamente routers

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,8 +1,4 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import FeaturesPage from '../pages/landingPages/FeaturesPage'
-import MisVisPage from '../pages/landingPages/MisVisPage'
-import HomePage from '../pages/landingPages/HomePage'
-import ContactPage from '../pages/landingPages/ContactPage'
 import LoginPage from '../pages/landingPages/LoginPage'
 import Landing from '../pages/landingPages/Landing'
 import listarBusesPage from '../pages/adminPages/listarBusesPage'
@@ -13,10 +9,7 @@ export default function AppRouter() {
     return (
         <Router>
             <Switch>
-                <Route path="/home" component={HomePage}/>
-                <Route path="/misVis" component={MisVisPage}/>
-                <Route path="/features" component={FeaturesPage}/>
-                <Route path="/contact" component={ContactPage}/>    
+                <Route path="/home" component={Landing}/>   
                 <Route path="/login" component={LoginPage}/>            
                 <Route path="/listarBuses" component={listarBusesPage}/>
                 {/* <Route path="/listarVuelta" component={listarVueltaPage}/>
